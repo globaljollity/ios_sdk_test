b0db5b5da539f2d59830a4a853ca74b08c9a9a93
globaljollity <globaljollity@gmail.com>
2025-11-07 14:47:26 +0800

    2.0.5
    测试Apple内购掉单问题

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "oneSDK",
+    products: [
+        .library(
+            name: "oneSDK",
+            targets: ["oneSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "14.1.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0"),
+        .package(url: "https://github.com/adjust/ios_sdk.git", from: "5.4.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "oneSDKBranch",
+            url: "https://github.com/globaljollity/ios_sdk_test/raw/main/oneSDK.xcframework.zip",
+            checksum: "aa66df7db160c163bbf9da97dfa36ce8a0ea9613ffbff3925d5f9ec433f06799"
+        ),
+        .target(
+            name: "oneSDK",
+            dependencies: [
+                "oneSDKBranch",
+                .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
+                .product(name: "FacebookBasics", package: "facebook-ios-sdk"),
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
+                .product(name: "FacebookShare", package: "facebook-ios-sdk"),
+                .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
+                .product(name: "AdjustGoogleOdm", package: "ios_sdk"),
+                .product(name: "AdjustSdk", package: "ios_sdk"),
+                .product(name: "AdjustWebBridge", package: "ios_sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
+            ]
+        ),
+        .testTarget(
+            name: "oneSDKTests",
+            dependencies: ["oneSDK"]
+        ),
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ios_sdk_test
+# 本SDK已集成Facebook登录、Google登录、苹果登录、CIB ID登录、Adjust广告植入  
+Facebook版本14.1.0  
+Google版本8.0.0  
+Adjust版本5.4.0  
+
+未经授权, 禁止使用  
+
+## Version 2.0.5  
+测试Apple内购掉单问题
