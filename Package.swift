483eb43bdcdddc57946a23d2aab5466efb696347
globaljollity <globaljollity@gmail.com>
2025-12-03 10:19:07 +0800

    2.0.28
    添加-ObjC参数

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["oneSDK"]),
     ],
     dependencies: [
-        // .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "9.0.0"),
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "9.0.0"),
         .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0"),
         .package(url: "https://github.com/adjust/ios_sdk.git", from: "5.4.0")
     ],
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "oneSDKBranch",
             url: "https://github.com/globaljollity/ios_sdk_test/raw/main/oneSDK.xcframework.zip",
-            checksum: "a2401098d70484d549e156c0a3cc65c61bf8e2baace04f8e204cfe7be118951d"
+            checksum: "28b3c89dd5625aef6ae7896c69924a74fec3f6d20fdbcb07dcbad7eb6bef34e1"
         ),
         .target(
             name: "oneSDK",
@@ -27,10 +27,10 @@ let package = Package(
                 "oneSDKBranch",
 //                .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
 //                .product(name: "FacebookBasics", package: "facebook-ios-sdk"),
-//                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
-//                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
-//                .product(name: "FacebookShare", package: "facebook-ios-sdk"),
-//                .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
+                .product(name: "FacebookShare", package: "facebook-ios-sdk"),
+                .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
                 .product(name: "AdjustGoogleOdm", package: "ios_sdk"),
                 .product(name: "AdjustSdk", package: "ios_sdk"),
                 .product(name: "AdjustWebBridge", package: "ios_sdk"),
