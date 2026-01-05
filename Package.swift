6f6dc5ad9fa3d0f5bbeffeedbcc47453380b11e5
globaljollity <globaljollity@gmail.com>
2026-01-05 17:13:33 +0800

    2.0.43
    适配新环境10

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
             targets: ["oneSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "9.0.0"),
-        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0"),
-        .package(url: "https://github.com/adjust/ios_sdk.git", from: "5.4.0")
     ],
     targets: [
         .binaryTarget(
@@ -24,24 +21,7 @@ let package = Package(
         .target(
             name: "oneSDK",
             dependencies: [
-                "oneSDKBranch",
-                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
-                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
-                .product(name: "FacebookShare", package: "facebook-ios-sdk"),
-                .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
-                .product(name: "AdjustGoogleOdm", package: "ios_sdk"),
-                .product(name: "AdjustSdk", package: "ios_sdk"),
-                .product(name: "AdjustWebBridge", package: "ios_sdk"),
-                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
-                .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
-            ],
-            linkerSettings: [
-                .linkedFramework("Security"),
-                .linkedFramework("Accounts"),
-                .linkedFramework("Social"),
-                .linkedFramework("CoreTelephony"),
-                .linkedFramework("SystemConfiguration"),
-                .linkedFramework("WebKit")
+                "oneSDKBranch"
             ]
         ),
         .testTarget(
