225d70eafa035fa21186fbd326e2bef2fbfd4d62
globaljollity <globaljollity@gmail.com>
2026-01-05 12:11:54 +0800

    2.0.34
    适配新环境

--- Package.swift
+++ Package.swift
@@ -8,32 +8,35 @@ let package = Package(
     products: [
         .library(
             name: "oneSDK",
-            type: .static, // 静态库合并符号，避免外部暴露
-            targets: ["oneSDK"]
-        ),
+            targets: ["oneSDK"]),
     ],
     dependencies: [
-        // 【彻底清空】：删除所有 Facebook、GoogleSignIn、Adjust 的依赖声明
-        // 不再通过 SPM 拉取任何第三方库，完全复用 xcframework 内置版本
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "9.0.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0"),
+        .package(url: "https://github.com/adjust/ios_sdk.git", from: "5.4.0")
     ],
     targets: [
         .binaryTarget(
             name: "oneSDKBranch",
             url: "https://github.com/globaljollity/ios_sdk_test/raw/main/oneSDK.xcframework.zip",
-            checksum: "6c90d72c3561abc50db26576f5f41516a061bbd90369e2e666c9350bc8caa2af"
+            checksum: "71d4598e666087ae3db59924aca14dc9be6b7110a9991cea6c7cd7c2e0158781"
         ),
         .target(
             name: "oneSDK",
             dependencies: [
-                "oneSDKBranch" // 【仅保留这个依赖】：无其他任何第三方 .product 依赖
-            ],
-//            // 符号隐藏配置：确保内置第三方库符号不对外暴露
-//            swiftSettings: [
-//                .unsafeFlags(["-Xfrontend", "-visibility=hidden"])
-//            ],
-//            linkerSettings: [
-//                .unsafeFlags(["-Xlinker", "-dead_strip"])
-//            ]
+                "oneSDKBranch",
+//                .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
+//                .product(name: "FacebookBasics", package: "facebook-ios-sdk"),
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
+                .product(name: "FacebookShare", package: "facebook-ios-sdk"),
+                .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
+                .product(name: "AdjustGoogleOdm", package: "ios_sdk"),
+                .product(name: "AdjustSdk", package: "ios_sdk"),
+                .product(name: "AdjustWebBridge", package: "ios_sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
+            ]
         ),
         .testTarget(
             name: "oneSDKTests",
